2e98895fbb18a1f4e0a66c1a441a278ebe3566f9
Gabriel Henrique <henrique.biel9753@gmail.com>
2023-05-10 22:36:16 -0300

    Commit da Logica de js

---
+++ crip.js
@@ -0,0 +1,49 @@
+var texto = document.getElementById("entrada");
+var saida = document.getElementById("saida");
+
+var entrada = "";
+
+
+var textodois="";
+
+var textotres="";
+
+var textoquatro="";
+
+var textocinco="";
+
+var textoseis= "";
+
+
+function criptografia() {
+            
+            entrada = texto.value;
+           
+            textodois = entrada.replace(/i/gi, "ime");
+            textotres = textodois.replace(/e/gi, "enter");
+            textoquatro = textotres.replace(/a/gi, "ai");
+            textocinco = textoquatro.replace(/o/gi, "ober");
+            textoseis = textocinco.replace(/u/gi, "ufat");
+           return document.getElementById("saida").value = textoseis;
+}
+
+function descriptografia() {
+            
+    entrada = texto.value;
+   
+    textodois = entrada.replace(/ai/gi, "a");
+    textotres = textodois.replace(/enter/gi, "e");
+    textoquatro = textotres.replace(/ime/gi, "i");
+    textocinco = textoquatro.replace(/ober/gi, "o");
+    textoseis = textocinco.replace(/ufat/gi, "u");
+
+   return document.getElementById("saida").value = textoseis;
+}
+
+
+var btnCopia = document.querySelector("#copiar");
+
+btnCopia.addEventListener('click', (e) => {
+    navigator.clipboard.writeText(saida.value);
+    document.getElementById('saida').value = "";
+})
