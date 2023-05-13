02c79ed4d067709455e892259d4cade3e6e783a4
Gabriel Henrique <henrique.biel9753@gmail.com>
2023-05-12 22:29:04 -0300

    arrumando o sistema de sumir e aparecer div

--- crip.js
+++ crip.js
@@ -16,7 +16,8 @@ var textoseis= "";
 
 
 function criptografia() {
-            
+            document.getElementById("terceiro").style.visibility = 'hidden';
+            document.getElementById("secundario").style.visibility = 'visible'
             entrada = texto.value;
            
             textodois = entrada.replace(/i/gi, "ime");
@@ -25,10 +26,14 @@ function criptografia() {
             textocinco = textoquatro.replace(/o/gi, "ober");
             textoseis = textocinco.replace(/u/gi, "ufat");
            return document.getElementById("saida").value = textoseis;
+
 }
 
 function descriptografia() {
-            
+
+    document.getElementById("terceiro").style.visibility = 'hidden';
+    document.getElementById("secundario").style.visibility = 'visible'
+
     entrada = texto.value;
    
     textodois = entrada.replace(/ai/gi, "a");
@@ -46,4 +51,5 @@ var btnCopia = document.querySelector("#copiar");
 btnCopia.addEventListener('click', (e) => {
     navigator.clipboard.writeText(saida.value);
     document.getElementById('saida').value = "";
+    document.getElementById('entrada').value ="";
 })
